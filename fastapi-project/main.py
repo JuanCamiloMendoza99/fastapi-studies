d44aaa50d76fbc0944f9fd6e28e310136f3e0dac
Juan Camilo Mendoza <juan.camilo.mendoza99@gmail.com>
2025-09-23 20:30:29 -0500

    Add timezone functionality and update dependencies

--- fastapi-project/main.py
+++ fastapi-project/main.py
@@ -1,13 +1,35 @@
 from fastapi import FastAPI
+from datetime import datetime
+import zoneinfo
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"message": "Hello world 🚀"}
 
 
 @app.get("/user/{name}")
-def read_item(name: str):
+async def read_item(name: str):
     return {"message": f"Hello {name}, welcome to FastAPI"}
+
+
+country_timezones = {
+    "CO": "America/Bogota",
+    "US": "America/New_York",
+    "FR": "Europe/Paris",
+    "IN": "Asia/Kolkata",
+}
+
+
+@app.get("/time/{iso_code}")
+async def get_time(iso_code: str):
+    if not iso_code:
+        return {"error": "ISO code is required"}
+
+    timezone = country_timezones.get(iso_code.upper())
+    if not timezone:
+        return {"error": "Country not found"}
+
+    return {"time": datetime.now(zoneinfo.ZoneInfo(timezone))}
