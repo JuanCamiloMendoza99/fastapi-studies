e3eec5a73a080ba721aece71be8a8c585d5bff59
Juan Camilo Mendoza <juan.camilo.mendoza99@gmail.com>
2025-09-26 18:39:16 -0500

    Add routers for invoices and transactions with respective endpoints

--- fastapi-project/app/main.py
+++ fastapi-project/app/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
 from datetime import datetime
 import zoneinfo
-from models import Transaction, Invoice
 from db import create_all_tables
 from .routers import customers
+from .routers import invoices
+from .routers import transactions
 
 
 app = FastAPI(lifespan=create_all_tables)
 app.include_router(customers.router)
+app.include_router(invoices.router)
+app.include_router(transactions.router)
 
 
 @app.get("/")
@@ -38,13 +41,3 @@ async def get_time(iso_code: str):
         return {"error": "Country not found"}
 
     return {"time": datetime.now(zoneinfo.ZoneInfo(timezone))}
-
-
-@app.post("/transactions")
-async def create_transaction(transaction: Transaction):
-    return transaction
-
-
-@app.post("/invoices")
-async def create_invoice(invoice: Invoice):
-    return invoice
